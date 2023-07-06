d014e6090b118ee2388dcc34a1189593549fead5
Ssemaganda Elvis <42653286+elvisdouglas@users.noreply.github.com>
2023-07-05 18:32:20 -0700

    change password and timer count down

--- admins/change_p.php
+++ admins/change_p.php
@@ -7,25 +7,48 @@ session_start();
 $conn = require __DIR__ . "/conn.php";
 
 if(isset($_POST['submit'])){
+    $user_id = $_POST['user_id'];
     $email = $_POST['email'];
-    $password = $_POST['password'];
+    $op = $_POST['password'];
     $n_p = $_POST['n_p'];
     $c_p = $_POST['c_p'];
 
-    echo $password;
-    $sql = mysqli_query($conn, "SELECT email, password_hash FROM register WHERE email = '$email' AND password_hash = '$password'");
-
-    $num = mysqli_fetch_array($sql);
-
-    if($num > 0){
-        $con = mysqli_query($conn, "UPDATE register set password_hash = '$n_p' WHERE email = '$email'");
-        echo ("password updated");
+    if($n_p !== $c_p){
+        echo "the new passwords do not match";
         header("refresh:2;url=forgot_password.php");
-    }else{
-        echo ("password not updated");
-        header("refresh:2;url=admins.php");
     }
+    
+    $sql = "SELECT password_hash FROM register WHERE password_hash = '$op'";
+    $result = mysqli_query($conn, $sql);
+    
+    if(mysqli_num_rows($result) == 1){
+        $sql2 = "UPDATE register SET password_harsh = '$n_p' WHERE id = '$user_id'";
+        $result2 = mysqli_query($conn, $sql2);
+        echo "Password changed successfully";
+        header("refresh:2;url=forgot_password.php");
+        exit();
+    }else{
+        echo "incorrect password";
+        header("refresh:2;url=forgot_password.php");
+        exit();
+    }
+
 }
 
 
+
+
+//$num = mysqli_fetch_array($sql);
+
+//if($num > 0){
+    //$con = mysqli_query($conn, "UPDATE register set password_hash = '$n_p' WHERE email = '$email'");
+    //echo ("password updated");
+    //header("refresh:2;url=forgot_password.php");
+    //}else{
+    //echo ("password not updated");
+    //    header("refresh:2;url=admins.php");
+//}
+
+
 ?>
+
