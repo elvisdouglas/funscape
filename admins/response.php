e897537d72866871e52cf2ab03558317aeac404d
Ssemaganda Elvis <42653286+elvisdouglas@users.noreply.github.com>
2023-07-07 17:44:20 -0700

    countdown script

--- admins/response.php
+++ admins/response.php
@@ -2,27 +2,47 @@
 session_start();
 
 
-$from_time1=date('H:i:s');
-//$to_time1=$_SESSION["duration"];
-$_SESSION["end_time"] = strtotime('+ 5 minute');
+//$from_time1=date('H:i:s');
+$to_time1=$_SESSION["end-time"];
+//echo $to_time1;
+//$_SESSION["end_time"] = strtotime(' 5 minute');
+$currentTimestamp = time();
+$end_Time = strtotime("+ $to_time1 minute");
+ 
 
-if (isset($_SESSION["end_time"])) {
-    $to_time1 = $_SESSION["end_time"];  
+if($end_Time !== false){
+    $remaining_time = $end_Time - $currentTimestamp;
 
+    if($remaining_time > 0){
+        $hours = floor($remaining_time / 3600);
+        $minutes = floor(($remaining_time % 3600) / 60);
+        $seconds = $remaining_time % 60;
+        
+        echo "Time remaining: $hours hours, $minutes minutes, $seconds seconds";
+    }else{
+        echo "Countdown expired";
+    }
+}else{
+    echo "Invalid duration Format";
+}
+
+//if (isset($_SESSION["end_time"])) {
+  //  $to_time1 = $_SESSION["end_time"];  
+    //echo  $to_time1;
     //$end_t = $from_time1 + $to_time1;
 
     //$Cut = strtotime($end_t);
-    $time_first = strtotime($from_time1);
-    $time_second = $to_time1;
+   // $time_first = strtotime($from_time1);
+    //$time_second = $to_time1;
 
-    $dfs = strtotime("$time_second - $time_first");
-    //$difference_in_seconds = $time_first - $time_second;
+    //$dfs = strtotime("$time_second - $time_first");
+    //$difference_in_seconds = $time_second - $time_first;
 
-    echo gmdate('H:i:s', $dfs);
+    //echo gmdate('H:i:s', $difference_in_seconds);
     //echo gmdate("H:i:s", $difference_in_seconds);
-} else {
-    echo "The 'end_time' session variable is not set.";
-}
+//} else {
+  //  echo "The 'end_time' session variable is not set.";
+//}
 
 
 //$adding= strtotime("17:26:11 + $duration minute");
