030ee4ea9a0414813e2c3c1a57c3569fe419d6ee
Ssemaganda Elvis <42653286+elvisdouglas@users.noreply.github.com>
2023-07-17 18:31:10 -0700

    forgot password function

--- admins/forgot_password.php
+++ admins/forgot_password.php
@@ -9,11 +9,19 @@ include 'conn.php';
 
 $conn = require __DIR__ . "/conn.php";
 
+$use = "SELECT * FROM register where username = '".$_SESSION['user_id']."' ";
+
+
+// $use = "SELECT * FROM register ";
+$c_use = mysqli_query($conn,$use);
+$row=mysqli_fetch_assoc($c_use);
+    $id = $_SESSION['user_id'];
+    //echo $_SESSION["end-time"];
 ?>
 
 
 <div class="container-fluid">
-
+    
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
@@ -25,12 +33,12 @@ $conn = require __DIR__ . "/conn.php";
             
     <form action="change_p.php" method="post">
         <div class="row">
-        <input type="hidden" name="user_id" id="user_id" value="">
-            <div class="col-md-6 mb-3">
+        <input type="hidden" name="id" id="user_id" value="<?php echo $_SESSION['user_id'] ?>" />
+            <!-- <div class="col-md-6 mb-3">
                 <label for="">Email</label>
                 <input type="text" name="email" id="user_email"  class="form-control" required>
-            </div>
-            <div class="col-md-6 mb-3">
+            </div> -->
+            <div class="col-md-6 mb-3"> 
                 <label for="">Current Password</label>
                 <input type="password" name="password" id="password"  class="form-control" required>
             </div>
@@ -38,10 +46,10 @@ $conn = require __DIR__ . "/conn.php";
                 <label for="">New password</label>
                 <input type="password" name="n_p" id="n_p" class="form-control" required>
             </div>
-            <div class="col-md-6 mb-3">
+            <!-- <div class="col-md-6 mb-3">
                 <label for="">Confirm Password</label>
                 <input type="password" name="c_p" id="c_p" class="form-control" required>
-            </div>
+            </div> -->
             <div class="col-md-12 mb-3">
                 <button type="submit" name="submit" value="change password" class="btn btn-success">Change Password</button>
             </div>
