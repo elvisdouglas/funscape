e4c21d4f4c2120eb4d4512f6567afbbdde8d2f4b
Ssemaganda Elvis <42653286+elvisdouglas@users.noreply.github.com>
2023-08-10 16:12:00 -0700

    changing forgot password to change password

--- admins/includes/navbar.php
+++ admins/includes/navbar.php
@@ -88,7 +88,7 @@
       </a>
       <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
-          <a class="collapse-item" href="forgot_password.php">Forgot Password</a>
+          <a class="collapse-item" href="forgot_password.php">Change Password</a>
         </div>
       </div>
     </li>
